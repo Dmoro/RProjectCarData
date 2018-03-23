8e207160e19be9f31eebc8bd06ba6f168940de52
Daniele Moro <moro.daniele@gmail.com>
2018-03-22 20:14:45 -0600

    actual code

--- CCtesting.R
+++ CCtesting.R
@@ -8,43 +8,48 @@ splitData <- function(df, percent) {
   set.seed(123)
   train_ind <- sample(seq_len(nrow(df)), size = smp_size)
   
-  train_df <- df[train_ind, ]
-  test_df <- df[-train_ind, ]
+  train_df <-  data.frame(df[train_ind, ])
+  test_df <- data.frame(df[-train_ind, ])
+  
   return(list("train" = data.matrix(train_df), "test" = data.matrix(test_df)))
 }
 
 
-setwd("C:/Users/Rum/OneDrive/School/Boise State University/CS354/Project")
 data = read.csv("carCrashData.csv")
 
-x_data = subset(data, select=c("dvcat","weight","airbag","seatbelt","frontal","sex","ageOFocc","yearacc","yearVeh","abcat","occRole","deploy","injSeverity"))
+x_data = subset(data, select=c("dvcat","weight","airbag","seatbelt","frontal","sex",
+                               "ageOFocc","yearacc","yearVeh","abcat","occRole","deploy","injSeverity"))
 y_data = subset(data, select=c("dead"))
 ncol(y_data)
 
-
+#split the data amont training and testing
 x_data = splitData(x_data, 0.8)
-y_data = splitData(y_data, 0.8)
+y_data = splitData(y_data, 0.8) 
 
-#nrow(x_data$train)
-#nrow(x_data$test)
-#length(y_data$train)
-#length(y_data$test)
+#convert the y data to a one dimensional array
+y_data$train = as.vector(y_data$train)
+y_data$test = as.vector(y_data$test)
 
-#print(x_data$train[978,])
-#print(y_data$train[978])
 
-print(class(y_data$train))
-y_data$train <- to_categorical(y_data$train, 2)
-y_data$test <- to_categorical(y_data$test, 2)
+#convert one dimensional array to a categorical matrix
+y_data$train <- to_categorical(y_data$train, 3)
+y_data$train <-y_data$train[,-1]
+y_data$test <- to_categorical(y_data$test, 3)
+y_data$test <-y_data$test[,-1]
+
 print(y_data$train)
+print(y_data$test)
+print(x_data$train)
+print(x_data$test)
+
 
 #model
 model <- keras_model_sequential()
 model %>%
   layer_dense(units = 1000, activation = 'relu', input_shape = c(ncol(x_data$train))) %>%
-  #layer_dropout(rate = 0.4) %>%
-  #layer_dense(units = 128, activation = 'relu') %>%
-  #layer_dropout(rate = 0.3) %>%
+  layer_dropout(rate = 0.4) %>%
+  layer_dense(units = 128, activation = 'relu') %>%
+  layer_dropout(rate = 0.3) %>%
   layer_dense(units = 2, activation = 'softmax')
 
 summary(model)
@@ -68,7 +73,16 @@ plot(history)
 model %>% evaluate(x_data$test, y_data$test)
 
 y_prediction = model %>% predict_classes(x_data$test)
+print(y_prediction)
 
+print(x_data$train[1, ])
+print(y_data$train[1, ])
 
+B = matrix( 
+  c( 2, 19.287, 2,1, 1,1,80,1998,1981,3,1,0,3), 
+  nrow=1, 
+  ncol=13) 
+print(B)
 
+print(model %>% predict_classes(B))
 
