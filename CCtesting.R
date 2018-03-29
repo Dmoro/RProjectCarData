778d88802138b1703648d092801133343dfa3572
Daniele Moro <moro.daniele@gmail.com>
2018-03-29 11:21:49 -0600

    fixed data

--- CCtesting.R
+++ CCtesting.R
@@ -1,15 +1,9 @@
 library(keras)
 
-splitData <- function(df, percent) {
-  ## 75% of the sample size
-  smp_size <- floor(percent * nrow(df))
-  
-  ## set the seed to make your partition reproductible
-  set.seed(123)
-  train_ind <- sample(seq_len(nrow(df)), size = smp_size)
-  
+splitData <- function(df, percent, sample) {
+  train_ind <- sample
   train_df <-  data.frame(df[train_ind, ])
-  test_df <- data.frame(df[-train_ind, ])
+  test_df <- data.frame(df[train_ind, ])
   
   return(list("train" = data.matrix(train_df), "test" = data.matrix(test_df)))
 }
@@ -18,18 +12,27 @@ splitData <- function(df, percent) {
 
 # Cleaned carCrashData to convert severity 5 to 0, 6 to 4, and NA's to 0.
 data = read.csv("carCrashDataCleaned.csv")
+print(data)
 
-#x_data = subset(data, select=c("dvcat","weight","airbag","seatbelt","frontal","sex",
-#                              "ageOFocc","yearacc","yearVeh","abcat","occRole","deploy","injSeverity"))
+x_data = subset(data, select=c("dvcat","weight","airbag","seatbelt","frontal","sex",
+                              "ageOFocc","yearacc","yearVeh","abcat","occRole","deploy"))
 
-x_data = subset(data, select=c("dvcat", "dead", "airbag","seatbelt","frontal","sex","abcat","occRole","deploy"))
+#x_data = subset(data, select=c("dvcat", "dead", "airbag","seatbelt","frontal","sex","abcat","occRole","deploy"))
 
 y_data = subset(data, select=c("injSeverity"))
 nrow(y_data) # equals 26,217 colums with row injSeverity
 
+print(x_data)
+print(y_data)
+
+
 #split the data into list(matrix,matrix) with 80% training and 20% testing respectively.
-x_data = splitData(x_data, 0.8)
-y_data = splitData(y_data, 0.8) 
+set.seed(123)
+percent = 0.7
+smp_size <- floor(percent * nrow(x_data))
+sample = sample(seq_len(nrow(x_data)), size = smp_size)
+x_data = splitData(x_data, 0.7, sample)
+y_data = splitData(y_data, 0.7, sample) 
 
 
 #convert the y data to a one dimensional array
@@ -39,6 +42,10 @@ y_data$test = as.vector(y_data$test)
 #convert one dimensional array to a categorical matrix via one-hot encoding
 y_data$train <- to_categorical(y_data$train, 5)
 y_data$test <- to_categorical(y_data$test, 5)
+
+print(x_data$train[1,])
+print(x_data$train[1:10,])
+print(y_data$test[1:10,])
 
 #model
 model <- keras_model_sequential()
@@ -50,6 +57,7 @@ model %>%
   #layer_dropout(rate = 0.3) %>%
   layer_dense(units = 100, activation = 'relu') %>%
   layer_dense(units = 100, activation = 'relu') %>%
+
   layer_dense(units = 5, activation = 'softmax') # increased our units to 5 to reflect all categories of injSeverity
 
 summary(model)
@@ -68,7 +76,7 @@ print(x_data)
 #train and eval
 history <- model %>% fit(
   x_data$train, y_data$train,
-  epochs = 30, batch_size = 128,
+  epochs = 100, batch_size = 100,
   validation_split = 0.2
 )
 
